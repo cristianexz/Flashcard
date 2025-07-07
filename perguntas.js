107d10f3809e3f04e1cf070efef6c585ce96f4eb
cristianexz <chaves.cristiane@escola.pr.gov.br>
2025-07-07 11:33:12 -0300

    htrtgf

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,17 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    Fórmula 1,
+    Quem venceu o GP Brasil 1993?,
+    Ayrton Senna
+)
+
+criaCartao(
+    Fórmula 1,
+    Quem é o piloto com mais títulos consecutivos (2020)?,
+    Max Verstappen
+)
+
+criaCartao(
+    Fórmula 1,
+    Quantos brasileiros correram e quantos venceram?,
+    33 correram e apenas 6 venceram
 )
