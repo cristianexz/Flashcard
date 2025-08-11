ff15d3eff959aa686640c34180eca7a58d7771da
cristianexz <chaves.cristiane@escola.pr.gov.br>
2025-08-11 08:44:36 -0300

    nv b

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-    ´Fórmula 1`
-    ´Quem venceu o GP Brasil 1993?`,
-    ´Ayrton Senna`
+    'Fórmula 1`
+    'Quem venceu o GP Brasil 1993?`,
+    'Ayrton Senna`
 )
 
 criaCartao(
-    ´Fórmula 1,`
-    ´Quem é o piloto com mais títulos consecutivos (2020)?`,
-    ´Max Verstappen`
+    'Fórmula 1,`
+    'Quem é o piloto com mais títulos consecutivos (2020)?`,
+    'Max Verstappen`
 )
 
 criaCartao(
-    ´Fórmula 1`,
-    ´Quantos brasileiros correram e quantos venceram?`,
-    ´33 correram e apenas 6 venceram`,
+    'Fórmula 1`,
+    'Quantos brasileiros correram e quantos venceram?`,
+    '33 correram e apenas 6 venceram`,
 )
