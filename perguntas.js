cddb45d2e9eee592891afd1f0ac33bb059b83756
cristianexz <chaves.cristiane@escola.pr.gov.br>
2025-08-11 09:05:27 -0300

    j556tr

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,29 @@
 criaCartao(
-    'Fórmula 1`
-    'Quem venceu o GP Brasil 1993?`,
-    'Ayrton Senna`
+    'Fórmula 1'
+    'Quem venceu o GP Brasil 1993?',
+    'Ayrton Senna'
 )
 
 criaCartao(
-    'Fórmula 1,`
-    'Quem é o piloto com mais títulos consecutivos (2020)?`,
-    'Max Verstappen`
+    'Fórmula 1,'
+    'Quem é o piloto com mais títulos consecutivos (2020)?',
+    'Max Verstappen'
 )
 
 criaCartao(
-    'Fórmula 1`,
-    'Quantos brasileiros correram e quantos venceram?`,
-    '33 correram e apenas 6 venceram`,
+    'Fórmula 1',
+    'Quantos brasileiros correram e quantos venceram?',
+    '33 correram e apenas 6 venceram',
 ) 
 
 criaCartao(
-    'Fórmula 1`,
-    'Qual a corrida mais glamourosa do calendário?`,
-    ' Mônaco`,
+    'Fórmula 1',
+    'Qual a corrida mais glamourosa do calendário?',
+    ' Mônaco',
 ) 
 
 criaCartao(
-    'Fórmula 1`,
-    'Qual o orgão responsável pela categoria?`,
-    ' FIA, Federação Internacional do Automobilismo`,
+    'Fórmula 1',
+    'Qual o orgão responsável pela categoria?',
+    ' FIA, Federação Internacional do Automobilismo',
 ) 
